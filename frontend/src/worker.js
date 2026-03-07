2a60da5bb4477dcf82134f8cd0aab1ba16f17181
aaronj <aaron@aaronjenkins.net>
2026-03-07 12:28:04 -0500

    Remove UI code (moved to beholder-ui repo)

--- frontend/src/worker.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env) {
-    // `ASSETS` binding is provided by Wrangler/Pages when `assets.directory` is set.
-    // This minimal worker simply delegates requests to the Pages assets binding.
-    return env.ASSETS.fetch(request);
-  }
-}
